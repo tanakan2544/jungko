b83d5f27ad7eb8fb5db2f41fa361bfde53d8a1ad
tanakan2544 <52315277+tanakan2544@users.noreply.github.com>
2019-07-05 16:18:36 +0700

    Add files via upload

---
+++ receipt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("CP ALL, 7-ELven  กอล์ฟวิลล์ (เชียงร(8903))")
+print(" TAK#0107542000011(VAT Included       )")
+print("VAT  06891 POS# :EO4131000201099    ")
+print("ใบเสร็จรับเงิน/ใบกำกับภาษีอย่างย่อ            ")
+print("1  ถั่วโก๋แก่โนริวาซาบิ"            ,1* 20.00,      "THB     ")
+print("1  มิรินด้าน้ำแดง "                   ,1*12.00,      "THB   ")
+print("TOtal        (2)           32.00                   ")
+print("Cash/Chang              40.00   8.00          ")
+print("R#0000358695 P2: 19/09/56  19.16       ")
+
+
+# In[ ]:
+
+
+
+
