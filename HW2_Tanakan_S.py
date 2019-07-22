e12d402830cfe3c2bb2cf850eeced2b2987ee71c
tanakan2544 <52315277+tanakan2544@users.noreply.github.com>
2019-07-22 12:15:02 +0700

    HW2

---
+++ HW2_Tanakan_S.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#นาย ธนาคาร สายรีรักษ์ 362515241006 EE36241N
+print("--------------Menu--------------")
+print("Welcome to Icecream Shop")
+print("1. Strawberry 80 THB")
+print("2. Vanila 50 THB")
+print("3. Chocolate 40 THB")
+print("4. Lemon 90 THB")
+print("5. Milk 10 THB")
+bbb=int(input("What do you want?"))
+if bbb==1 :
+    print("You order : 1 Strawberry 80 THB")
+elif bbb==2 :
+    print("You order : 2 Vanila 50 THB")
+elif bbb==3 :
+    print("You order : 3 Chocolate 40 THB")
+elif bbb==4 :
+    print("You order : 4 Lemon 90 THB")
+elif bbb==5 :
+    print("You order : 5 Milk 10 THB")
+
+
+# In[ ]:
+
+
+
+
